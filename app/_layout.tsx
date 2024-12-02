ad0a8e13c11ca2290e0667ee022e815b9ade5b31
seanchen0123 <270061968@qq.com>
2024-12-02 13:22:18 +0800

    nativewind init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import '@/assets/css/global.css'
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -30,10 +31,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
+import { useAssets } from "expo-asset"
 import { Text, View } from "react-native"
 
 const Page = () => {
 
+  const [asset] = useAssets([require('@/assets/videos/intro.mp4')])
+
   return (
     <View>
-      <Text>Index Page</Text>
+      <Text className="text-2xl font-bold underline text-rose-500">Index Page</Text>
     </View>
   )
 }
