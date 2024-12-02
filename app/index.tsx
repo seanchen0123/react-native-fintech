46db37036ce9108b396ce8b7b5412ec99d0f0b37
seanchen0123 <270061968@qq.com>
2024-12-02 15:19:36 +0800

    write index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,38 @@
-import { useAssets } from "expo-asset"
-import { Text, View } from "react-native"
+import { useAssets } from 'expo-asset'
+import { ResizeMode, Video } from 'expo-av'
+import { Link } from 'expo-router'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 const Page = () => {
-
-  const [asset] = useAssets([require('@/assets/videos/intro.mp4')])
+  const [assets] = useAssets([require('@/assets/videos/intro.mp4')])
 
   return (
-    <View>
-      <Text className="text-2xl font-bold underline text-rose-500">Index Page</Text>
+    <View className=" flex-1 justify-between relative">
+      {assets && (
+        <Video
+          source={{ uri: assets[0].uri }}
+          style={{ width: '100%', height: '100%', position: 'absolute' }}
+          resizeMode={ResizeMode.COVER}
+          isLooping
+          isMuted
+          shouldPlay
+        />
+      )}
+      <View className="mt-4 p-5">
+        <Text className="text-white text-3xl font-bold uppercase leading-normal">Ready to change the way you money?</Text>
+      </View>
+      <View className='mb-8 flex flex-row justify-center gap-5'>
+        <Link href={'/login'} asChild>
+          <TouchableOpacity className=' bg-white w-5/12 py-4 rounded-full'>
+            <Text className='text-zinc-900 font-semibold uppercase text-center'>Log in</Text>
+          </TouchableOpacity>
+        </Link>
+        <Link href={'/signup'} asChild>
+          <TouchableOpacity className=' bg-emerald-400 w-5/12 py-4 rounded-full'>
+            <Text className='text-white font-semibold uppercase text-center'>Sign up</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
     </View>
   )
 }
